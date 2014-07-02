b1028d99ba19a027ed99c40fd902795b39ab5b84
mlavin <markdlavin@gmail.com>
2014-07-01 22:22:32 -0400

    Cleaning up some whitespace and fixing test running on 1.5

--- qjoin/tests/test_models.py
+++ qjoin/tests/test_models.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+import re
+
 from django.test import TestCase, SimpleTestCase
  
 from .models import Person, Address, PersonAddress, Email
@@ -9,10 +11,16 @@ from ..models import QJoin, JoinExpression
 class QJoinSQLTestCase(SimpleTestCase):
     """Checking for the expected SQL generation."""
 
+    maxDiff = None
+
+    def _clean_sql(self, sql):
+        return re.sub(r'\s+', ' ', sql).strip()
+
     def assertQueryEqual(self, qs, sql):
         """Clean up newlines and leading spaces for asserting the SQL."""
-        expected = ''.join(map(lambda l: l.lstrip(' '), sql.split('\n')))
-        self.assertEqual(str(qs.query), expected)
+        result = self._clean_sql(str(qs.query))
+        expected = self._clean_sql(sql)
+        self.assertEqual(result, expected)
  
     def test_simple_join(self):
         """Build basic join."""
